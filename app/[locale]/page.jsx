bb36d86c9c7a56a8cd961603d4d73545734f8486
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-08-06 12:09:11 +0530

    `Refactor locale handling in Next.js application: update page component, remove LocaleSwitcher component, and modify locale files`

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,11 +1,16 @@
+export async function generateStaticParams() {
+  return ['en', 'hi', 'fr', 'de', 'es'].map((locale) => ({ locale }));
+}
+
 export default async function HomePage({ params }) {
-  const { locale } = params;
+  const locale = params?.locale;
+
   const messages = (await import(`../../locales/${locale}.json`)).default;
 
   return (
     <div>
       <h1>{messages.title}</h1>
-      <h1>{messages.description}</h1>
+      <p>{messages.description}</p>
     </div>
   );
 }
--- app/component/LocaleSwitcher.js
+++ app/component/LocaleSwitcher.js
@@ -1,27 +1,27 @@
-// 'use client';
+'use client';
 
-// import { usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
-// const locales = ['en', 'hi', 'fr', 'es', 'de'];
+const locales = ['en', 'hi', 'fr', 'es', 'de'];
 
-// export default function LanguageSwitcher() {
-//   const router = useRouter();
-//   const pathname = usePathname();
+export default function LanguageSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
 
-//   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     const newLocale = e.target.value;
-//     const segments = pathname.split('/');
-//     segments[1] = newLocale;
-//     router.push(segments.join('/'));
-//   };
+  const handleChange = (e) => {
+    const newLocale = e.target.value;
+    const segments = pathname.split('/');
+    segments[1] = newLocale;
+    router.push(segments.join('/'));
+  };
 
-//   return (
-//     <select onChange={handleChange} defaultValue={pathname.split('/')[1]}>
-//       {locales.map((loc) => (
-//         <option key={loc} value={loc}>
-//           {loc.toUpperCase()}
-//         </option>
-//       ))}
-//     </select>
-//   );
-// }
+  return (
+    <select onChange={handleChange} defaultValue={pathname.split('/')[1]}>
+      {locales.map((loc) => (
+        <option key={loc} value={loc}>
+          {loc.toUpperCase()}
+        </option>
+      ))}
+    </select>
+  );
+}
